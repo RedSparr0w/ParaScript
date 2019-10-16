566ef38028378a6738ade80fe0cee56706782742
RedSparr0w <admin@redsparr0w.com>
2019-10-16 21:59:05 +1300

    Update Bank.java

--- src/main/java/ParaScript/strategies/Bank.java
+++ src/main/java/ParaScript/strategies/Bank.java
@@ -2,6 +2,7 @@ package ParaScript.strategies;
 
 import ParaScript.data.Variables;
 import org.parabot.environment.api.utils.Time;
+import org.parabot.environment.input.Keyboard;
 import org.parabot.environment.scripts.framework.Strategy;
 import org.rev317.min.api.methods.*;
 import org.rev317.min.api.wrappers.Npc;
@@ -31,22 +32,18 @@ public class Bank implements Strategy {
     }
 
     public void depositItems() {
-        Npc banker[] = Npcs.getNearest(494);
-
-        if (banker != null) {
-            banker[0].interact(Npcs.Option.BANK);
-            Time.sleep(3000);
-            if (Game.getOpenInterfaceId() == 5292) {
-                if (Variables.skill_to_train == Skill.MINING)
-                    org.rev317.min.api.methods.Bank.depositAllExcept(1266, 1268, 1270, 1272, 1274, 1276);
-                if (Variables.skill_to_train == Skill.WOODCUTTING)
-                    org.rev317.min.api.methods.Bank.depositAllExcept(1350, 1352, 1354, 1356, 1358, 1360, 6740);
-                if (Variables.skill_to_train == null) {
-                    org.rev317.min.api.methods.Bank.depositAllExcept(1);
-                    Variables.addItemGained(28);
-                }
-                Variables.setStatus("none");
+        Keyboard.getInstance().sendKeys("::bank", true);
+        Time.sleep(3000);
+        if (Game.getOpenInterfaceId() == 5292) {
+            if (Variables.skill_to_train == Skill.MINING)
+                org.rev317.min.api.methods.Bank.depositAllExcept(1266, 1268, 1270, 1272, 1274, 1276);
+            else if (Variables.skill_to_train == Skill.WOODCUTTING)
+                org.rev317.min.api.methods.Bank.depositAllExcept(1350, 1352, 1354, 1356, 1358, 1360, 6740);
+            else {
+                org.rev317.min.api.methods.Bank.depositAllExcept(1);
+                Variables.addItemGained(28);
             }
+            Variables.setStatus("none");
         }
     }
 }
