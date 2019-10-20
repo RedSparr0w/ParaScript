60048b1368e9cefa79c42142000bb06947cf3353
RedSparr0w <admin@redsparr0w.com>
2019-10-21 09:29:01 +1300

    remove comment

--- src/main/java/ParaScript/data/variables/Npcs.java
+++ src/main/java/ParaScript/data/variables/Npcs.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Npcs {
-    MAN_WOMAN("Man & Woman", new int[]{1, 2, 3, 4, 5, 6}, 8); // Man and Woman
+    MAN_WOMAN("Man & Woman", new int[]{1, 2, 3, 4, 5, 6}, 8);
 
     private String name;
     private int[] ids;
