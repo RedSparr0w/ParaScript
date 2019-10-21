5f78bec553b3b1744c81aa5b5769fe500d3da259
RedSparr0w <admin@redsparr0w.com>
2019-10-21 16:56:36 +1300

    Fixup

--- src/main/java/ParaScript/ui/UI.java
+++ src/main/java/ParaScript/ui/UI.java
@@ -10,8 +10,7 @@ import org.rev317.min.api.wrappers.Player;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class UI extends JFrame {
     private final ButtonGroup woodcutOptionButtonGroup = new ButtonGroup();
