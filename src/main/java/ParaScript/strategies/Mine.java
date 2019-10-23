44b7d7cc65af09698e71d1fe382084a064dfa523
RedSparr0w <admin@redsparr0w.com>
2019-10-23 14:05:16 +1300

    Add more rocks/ores

--- src/main/java/ParaScript/strategies/Mine.java
+++ src/main/java/ParaScript/strategies/Mine.java
@@ -46,12 +46,6 @@ public class Mine implements Strategy {
 
     private SceneObject ore(){
         int[] ore_to_mine = Variables.mining_ore_selected.getIDs();
-        if (ore_to_mine == Ores.COPPER_TIN.getIDs()) {
-            if (Inventory.getCount(437) >= 14)
-                ore_to_mine = Ores.TIN.getIDs();
-            if (Inventory.getCount(439) >= 14)
-                ore_to_mine = Ores.COPPER.getIDs();
-        }
         for(SceneObject ore : SceneObjects.getNearest(ore_to_mine)){
             if(Variables.VARROCK_EAST_MINE_ZONE.inTheZoneObject(ore)) {
                 return ore;
